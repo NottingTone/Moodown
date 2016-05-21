85950bf994820437e2f867a35bf934a3cf847413
SEIAROTg <seiarotg@gmail.com>
2016-05-21 12:43:40 +0800

    add Symbol.iterator polyfill for Chrome 50-

---
+++ polyfill.js
@@ -0,0 +1,7 @@
+(function(){
+
+	// polyfille for Chrome 50-
+	NodeList.prototype[Symbol.iterator] = NodeList.prototype[Symbol.iterator] || Array.prototype[Symbol.iterator];
+	HTMLCollection.prototype[Symbol.iterator] = HTMLCollection.prototype[Symbol.iterator] || Array.prototype[Symbol.iterator];
+
+})();
