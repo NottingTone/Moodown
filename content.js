497425d07d7b516cd90f27ad9202fed95ae01a41
injuryholmes <injuryholmes@gmail.com>
2017-06-16 10:30:50 +0800

    modified folder2Files; add findLinks

--- content.js
+++ content.js
@@ -1,34 +1,3 @@
-function folder2Files () {
-    const els = document.querySelectorAll("#region-main a[href^='http://moodle.nottingham.ac.uk/']");
-    for (const el of els) {
-        if (el.querySelector("img") && getFileTypeByIcon(el) === 'folder') {
-            showChildFile(el);
-        }
-    }
-}
-
-function showChildFile(el) {
-    fetch(el.href, {credentials: "include"})
-        .then(response => response.text())
-        .then(function(text) {
-            let node = document.createElement("html");
-            node.innerHTML = text;
-            el.parentNode.appendChild(node.querySelector("#folder_tree0 > ul > li > ul"));
-        })
-        .catch(function(e) {
-            console.log("fetch fail", e);
-    });
-}
-
-function getFileTypeByIcon(iconURL) {
-    for (let [iconPattern, fileType] of ICON_TYPES) {
-        if (iconPattern.test(iconURL)) {
-            return fileType;
-        }
-    }
-    return 'other';
-}
-
 const ICON_TYPES = new Map([
     [/\/f\/pdf-24$/, 'pdf'],
     [/\/f\/document-24$/, 'doc'],
@@ -36,9 +5,87 @@ const ICON_TYPES = new Map([
     [/\/f\/spreadsheet-24$/, 'spreadsheet'],
     [/\/f\/calc-24$/, 'calc'],
     [/\/folder\//, 'folder'],
+    [/\/forum\//, 'forum'],
+    [/\/assign\//, 'assign'],
 ]);
 
 
-(function () {
-    folder2Files();
-})();
+async function folder2Files (folder) {
+    const ul = await findChildFiles(folder);
+    const bt = document.createElement('button');
+    if(ul) {
+        bt.innerText = "+";
+        folder.parentNode.appendChild(bt);
+        folder.parentNode.appendChild(ul);
+        ul.style.display = "none";
+        bt.addEventListener("click", function() {
+            if(ul.style.display === "" ) {
+                ul.style.display = "none";
+                bt.innerText = "+";
+            } else {
+                ul.style.display = "";
+                bt.innerText = "-";
+            }
+        });
+    } else {
+        bt.innerText = "null";
+        folder.parentNode.appendChild(bt);
+        bt.disabled = true;
+    }  
+}
+
+
+
+async function findChildFiles(folder) {
+    try {
+        const resp = await fetch(folder.href, {credentials: 'include'});
+        const html = await resp.text();
+        const doc = document.createElement('html');
+        doc.innerHTML = html;
+        return doc.querySelector('#folder_tree0 > ul > li > ul')
+    } catch (e) {
+        console.log('fetch fail', e);
+    }
+}
+
+
+function getFileTypeByIcon(iconURL) {
+    for (const [iconPattern, fileType] of ICON_TYPES) {
+        if (iconPattern.test(iconURL)) {
+            return fileType;
+        }
+    }
+    return 'other';
+}
+
+function enSelectable(el) {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    el.appendChild(checkbox);
+}
+
+
+async function findLinks() {
+    links = document.querySelectorAll("#region-main a[href^='http://moodle.nottingham.ac.uk/']");
+    for (const link of links) {
+        if(link.querySelector('img')) {
+            const fileType = getFileTypeByIcon(link);
+            if (fileType !== 'folder' && fileType !== 'assign' && fileType !== 'forum') {
+                enSelectable(link);
+            } else if (fileType === 'folder'){
+                await folder2Files(link);
+                if (link.parentNode.querySelector("button").innerText!=="null") {
+                    linksInFolder = link.parentNode.querySelectorAll("a[href^='http://moodle.nottingham.ac.uk/']");
+                    for (const innerLink of linksInFolder) {
+                        enSelectable(innerLink);
+                    }
+                }
+            }
+        } else {
+            enSelectable(link);
+        }
+    }
+}
+
+
+findLinks();
