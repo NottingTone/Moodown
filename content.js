c35d5a245b10c570ba698596e0176cb3add42589
injuryholmes <injuryholmes@gmail.com>
2017-06-27 14:55:48 +0800

    add structurlize function to find layer name by its tagname, and put the attribute into the original tree

--- content.js
+++ content.js
@@ -4,21 +4,77 @@ function isFile(href){
 
 function traversal(node) {
     if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'A' && isFile(node.href)) {
-        return node;
-    }
-
-    const subTrees = [];
-    for (const child of node.childNodes) {
-        const subTree = traversal(child);
-        if (subTree.length || subTree instanceof Node) {
-            subTrees.push(subTree);
+        return [node, []];
+    } else {
+        const subTrees = [];
+        for (const child of node.childNodes) {
+            const subTree = traversal(child);
+            if (subTree.length || subTree instanceof Node) {
+                subTrees.push(subTree);
+            }
         }
+        return [node, subTrees];
     }
-    return [node, subTrees];
 }
 
-const dom = traversal(document.querySelector('#region-main'));
-console.log(dom);
+
+function chooseFunc(node) {
+    function getNameByUl(ul) {
+        console.log('i am ul');
+        return 'ul';
+    }
+    function getNameByLi(li) {
+        console.log('i am li');
+        return 'li';
+    }
+    function getNameByP(p) {
+        console.log('i am p');
+        return 'p';
+    }
+    function foo() {
+        console.log("fofo");
+        return 'foo';
+    }
+    if (node.nodeType === Node.ELEMENT_NODE) {
+        const tag = node.tagName;
+        // console.log(tag);
+        if (tag === 'UL') {
+            return getNameByUl;
+        } else if (tag === 'LI') {
+            return getNameByLi;
+        } else if (tag === 'P') {
+            return getNameByP;
+        } else {
+            return foo;
+        }
+    }
+    return foo;
+}
 
 
 
+function getNodeName(node) {
+    const getNameFunc = chooseFunc(node);
+    return getNameFunc(node);
+}
+
+
+function structurlize() {
+    const sourceTree = traversal(document.querySelector('div.course-content'));
+    genStructure(sourceTree);
+    console.log(sourceTree);
+    return sourceTree;
+}
+
+
+function genStructure(curTree) {
+    const curParentNode = curTree[0];
+    const curSubTrees = curTree[1];
+    const layerName = getNodeName(curParentNode);
+    curTree.push(layerName);
+    for (const subTree of curSubTrees) {
+        genStructure(subTree);
+    }
+}
+
+structurlize();
